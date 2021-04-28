9d904a09873477880da9fd91a3b60a2b34b43289
konstantin <zakharchenko.konstantin@gmail.com>
2021-04-28 19:10:54 +0300

    remove get pips

--- src/investarena/components/CryptoBubbles/CryptoBubblesWrap.jsx
+++ src/investarena/components/CryptoBubbles/CryptoBubblesWrap.jsx
@@ -1,4 +1,4 @@
-import { getBubbleSizeList, getPips } from '../../components/CryptoBubbles/cryptoHelper';
+import { getBubbleSizeList } from '../../components/CryptoBubbles/cryptoHelper';
 import _isEmpty from 'lodash/isEmpty';
 import CryptoBubbles from '../../components/CryptoBubbles/CryptoBubbles';
 import React from 'react';
@@ -9,13 +9,12 @@ export const NO_DATA_VALUE = '-';
 
 const CryptoBubblesWrap = ({ openDeals, quotesSettings, quotes }) => {
     const pipsList = !_isEmpty(openDeals) && !_isEmpty(quotes) && !_isEmpty(quotesSettings) ? Object.values(openDeals).map(deal => {
-        const dealPips = getPips(deal, quotesSettings[deal.security], quotes[deal.security]);
         const getDealPnl = () => {
             let pnl = PlatformHelper.getPnl(quotes[deal.security], deal, quotesSettings[deal.security]);
             pnl = (isNaN(pnl) || pnl === undefined) ? NO_DATA_VALUE : parseFloat(pnl).toFixed(2);
             return pnl;
         };
-        return { ...deal, pips: dealPips, pnl: getDealPnl(), instrumentName: quotesSettings[deal.security].name };
+        return { ...deal, pnl: getDealPnl(), instrumentName: quotesSettings[deal.security].name };
     }) : null;
     const dealsWithBubbleSize = pipsList && getBubbleSizeList(pipsList);
     return <div className="st-crypto-bubbles">{ dealsWithBubbleSize ? <CryptoBubbles deals={dealsWithBubbleSize} /> : null }</div>;
